5bcef7a37df56076839e5b6468863d1ad41d82f4
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-05-04 16:44:47 +0100

    Improving comment

--- cron/download-shop-data.py
+++ cron/download-shop-data.py
@@ -1,5 +1,5 @@
 import requests, urllib
 
-# TODO:WV:20170504:I think this library may be out of date (XMLRPC only; not rest)  Check and remove if so.
+# TODO:WV:20170504:I think this library may be out of date (XMLRPC only; not REST)  Check and remove if so.
 from infusionsoft.library import Infusionsoft
 
