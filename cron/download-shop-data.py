d4de7c767426a5af491b9fa96a3ff058457e622f
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-05-04 16:44:04 +0100

    Move auth code out of cron job and into Flask

--- cron/download-shop-data.py
+++ cron/download-shop-data.py
@@ -1,20 +1,5 @@
 import requests, urllib
+
+# TODO:WV:20170504:I think this library may be out of date (XMLRPC only; not rest)  Check and remove if so.
 from infusionsoft.library import Infusionsoft
 
-
-# TODO:WV:20170503:These should not be in version control
-params = {
-	"response_type": "code",
-	"scope": "full",
-	"client_id": "",
-	"redirect_uri": "https://blueshift-app.willv.net/"
-}
-
-
-url = "https://signin.infusionsoft.com/app/oauth/authorize?"+urllib.urlencode(params);
-
-print "Visit the following URL in your browser: \n"
-print url
-print "\n"
-
-#print requests.get(url).content;
