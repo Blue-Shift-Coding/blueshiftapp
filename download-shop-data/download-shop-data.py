c4b1b13275b6d42a80bd7388d6ba41c008580ac2
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-05-05 15:00:32 +0100

    Adding to-do mark

--- download-shop-data/download-shop-data.py
+++ download-shop-data/download-shop-data.py
@@ -7,6 +7,7 @@ infusionsoftapi.refresh_access_token_data_if_necessary()
 
 products = infusionsoftapi.get_all_products()
 
+# TODO:WV:20170505:No categories in this data at the moment (although, there are subcategories, by ID only).  See if this can be fixed as the categories will be important for the website.
 f = open(products_file, "w")
 f.write(json.dumps(products))
 f.close()
