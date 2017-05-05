e0e24abf68852084d01eb9a058f1c3ec29f2340e
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-05-05 14:48:51 +0100

    Save products data to file after downloading

--- download-shop-data/download-shop-data.py
+++ download-shop-data/download-shop-data.py
@@ -1,11 +1,18 @@
 # To run this on the command line, use: 'python -m download-shop-data.download-shop-data.py'
-import infusionsoftapi
+import infusionsoftapi, pprint, json, os.path
+
+products_file = os.path.dirname(os.path.abspath(__file__))+"/products.json"
 
 infusionsoftapi.refresh_access_token_data_if_necessary()
 
 products = infusionsoftapi.get_all_products()
 
-print products
+f = open(products_file, "w")
+f.write(json.dumps(products))
+f.close()
+
+
+pprint.pprint(products)
 
 
 
