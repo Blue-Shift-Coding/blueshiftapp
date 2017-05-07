9d7aa688165724a6da443e653b94c669cd0a5b13
Richard Moss <richard@englishtutorslive.com>
2017-05-07 13:40:15 +0100

    temporily comments out infusionsoftapi __init__ for heroku deoploy and updates naming conventionsfor pages

--- infusionsoftapi/__init__.py
+++ infusionsoftapi/__init__.py
@@ -1,2 +1,2 @@
-from infusionsoftapi import get_authorization_url, download_initial_access_token_data, refresh_access_token_data_if_necessary
-from infusionsoftapi import get_all_products, get_product, get_event_keys, get_available_hooks, update_hook
+# from infusionsoftapi import get_authorization_url, download_initial_access_token_data, refresh_access_token_data_if_necessary
+# from infusionsoftapi import get_all_products, get_product, get_event_keys, get_available_hooks, update_hook
