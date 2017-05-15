02411186c27efa3ac195915585962cc496bd408a
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-05-15 18:18:09 +0100

    Adding dummy is-weekly data

--- shop_data/download.py
+++ shop_data/download.py
@@ -9,10 +9,10 @@ products = infusionsoftapi.get_all_products()
 # TODO:WV:20170515:Multiple date ranges per class
 # TODO:WV:20170515:Add actual data when  available
 dummy_data = {
-	1:{"start_date": None, "end_date": None, "min_age": None, "max_age": None},
-	3:{"start_date": 1496098800, "end_date": 1501714800, "min_age": 5, "max_age": 6},
-	5:{"start_date": 1496098800, "end_date": 1501714800, "min_age": 6, "max_age": 8},
-	7:{"start_date": 1496703600, "end_date": 1499382000, "min_age": None, "max_age": None}
+	1:{"start_date": None, "end_date": None, "min_age": None, "max_age": None, "is_weekly": False},
+	3:{"start_date": 1496098800, "end_date": 1501714800, "min_age": 5, "max_age": 6, "is_weekly": False},
+	5:{"start_date": 1496098800, "end_date": 1501714800, "min_age": 6, "max_age": 8, "is_weekly": False},
+	7:{"start_date": 1496703600, "end_date": 1499382000, "min_age": None, "max_age": None, "is_weekly": False}
 }
 def add_missing_data(product):
 	if product["id"] in dummy_data:
@@ -22,5 +22,3 @@ products["products"] = map(add_missing_data, products["products"])
 
 # TODO:WV:20170505:No categories in this data at the moment (although, there are subcategories, by ID only).  See if this can be fixed as the categories will be important for the website.
 storage.set("products", products)
-
-
