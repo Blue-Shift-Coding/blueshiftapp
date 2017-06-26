81dfc3a68432ab7b6f5f1473c7c39616899bb6a2
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-06-26 16:58:55 +0100

    Adding some behaviours to the shopping basket

--- static/js/classes.js
+++ static/js/classes.js
@@ -128,10 +128,30 @@
 
 
 /*
- * Product options
+ * Shopping basket
  */
 (function($) {
 	$(function() {
+
+		$(".update-shopping-basket-form").on("submit", function(e) {
+			var $form, $shoppingBasket, newQuantities = {};
+
+			$form = $(this);
+			$shoppingBasket = $form.closest(".container").find(".shopping-basket");
+			$shoppingBasket.find(".quantity-field").each(function() {
+				var $field = $(this);
+				newQuantities[$field.attr("data-product-id")] = $field.val();
+			});
+
+			$form.find("input[name=updates]").remove();
+			for (productId in newQuantities) {
+				var input;
+
+				input = $("<input type='hidden' name='updates' />")
+				input.val(productId+"|"+newQuantities[productId]);
+				$form.append(input);
+			}
+		});
 
 	});
 })(jQuery);
