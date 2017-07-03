7443fa509a6647a9a0da033095ea1cee4cb7eb31
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-07-03 11:09:44 +0100

    Working on display of gravity forms forms

--- app.py
+++ app.py
@@ -196,32 +196,59 @@ class BookingInformationFormBuilder():
             return u"<h"+heading_level+">"+text+"</h"+heading_level+">";
         return heading_widget
 
-    def get_radio_field(self, gf_field):
+    def get_option_field(self, field_type, gf_field, validators):
         choices = []
         for gf_choice in gf_field["choices"]:
             choices.append((gf_choice["value"], gf_choice["text"]+(" ("+gf_choice["price"]+")" if "price" in gf_choice and gf_choice["price"] != "" else "")))
-        return wtforms.RadioField(gf_field["label"], choices=choices)
+
+        print gf_field["label"]
+
+        if field_type == "radio":
+            print "Radio field"
+            return wtforms.RadioField(gf_field["label"], choices=choices, validators=validators)
+        else:
+            print "Select field"
+            return wtforms.SelectField(gf_field["label"], choices=choices, validators=validators)
+
+    def get_radio_field(self, gf_field, validators=[]):
+        return self.get_option_field("radio", gf_field, validators)
+
+    def get_select_field(self, gf_field, validators=[]):
+        return self.get_option_field("select", gf_field, validators)
 
     def build_booking_form(self):
         for gf_field in self.gravity_forms_data["fields"]:
             field_name = "gravity_forms_field_"+str(gf_field["id"])
+
+            validators = []
+            is_required = "isRequired" in gf_field and gf_field["isRequired"]
+            if is_required:
+                validators.append(wtforms.validators.Required())
+
             if gf_field["type"] == "section":
                 if "label" in gf_field and gf_field["label"] != "":
                     self.add_heading(gf_field["label"])
                 if "description" in gf_field and gf_field["description"] != "":
                     self.add_heading(gf_field["description"], "3")
+
             elif gf_field["type"] == "name":
                 self.add_heading(gf_field["label"], "4")
                 for sub_field in gf_field["inputs"]:
                     sub_field_name = field_name+"_"+str(sub_field["id"])
+
                     if "inputType"in sub_field and sub_field["inputType"] == "radio":
                         self.add_field(sub_field_name, self.get_radio_field(sub_field))
                     else:
                         self.add_field(sub_field_name, wtforms.StringField(sub_field["label"]))
+
+            elif gf_field["type"] == "select":
+                self.add_field(field_name, self.get_select_field(gf_field, validators))
+
             elif "inputType" in gf_field and gf_field["inputType"] == "radio":
-                self.add_field(field_name, self.get_radio_field(gf_field))
+                self.add_field(field_name, self.get_radio_field(gf_field, validators))
+
             else:
-                self.add_field(field_name, wtforms.StringField(gf_field["label"]))
+                self.add_field(field_name, wtforms.StringField(gf_field["label"], validators=validators))
 
         return self.form_class
 
