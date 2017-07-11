c152c9d759e03bf48c78cdfd56899970c78f7c40
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-07-11 12:39:03 +0100

    Add skeleton wordpress hook to prevent editing FILTERS category

--- wordpress-plugin/blueshift/blueshift.php
+++ wordpress-plugin/blueshift/blueshift.php
@@ -23,8 +23,15 @@ class BlueshiftPlugin {
 		}
 	}
 
+	public function preventAnyoneEditingTheFiltersCategory() {
+		add_action('edit_terms', function($termId, $termTaxonomyId, $taxonomySlug) {
+			// TODO:WV:20170711:Check for taxonomy=product_cat and name is FILTERS and no parent product_cats.  If so, set a flash message and redirect to index.
+		});
+	}
+
 	public function init() {
 		$this->stopGravityFormsAPIOverridingWordpressRestAPIs();
+		$this->preventAnyoneEditingTheFiltersCategory();
 	}
 }
 
