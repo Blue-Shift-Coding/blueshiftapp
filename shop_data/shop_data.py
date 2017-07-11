f0140cd174373661421e345caa2db9256e3db3d4
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-07-11 18:50:07 +0100

    Fleshing out the shop_data class by handling failure in various places

--- shop_data/shop_data.py
+++ shop_data/shop_data.py
@@ -21,8 +21,9 @@ def download_data():
 	expiry_time = time.time() + data_lifetime_in_seconds
 
 	# Get all forms from
-	# TODO:WV:20170629:Handle any error in 'get_forms'
 	forms = gf.get_forms().values()
+	if not isinstance(forms, list):
+		raise Exception("Forms could not be retrieved from stored data")
 	update_set("forms", expiry_time=expiry_time, item_ids=map(lambda x: x["id"], forms), get_item_method=gf.get_form)
 	update_set("categories", expiry_time=expiry_time, base_query="products/categories?")
 	update_set("products", expiry_time=expiry_time, base_query="products?")
@@ -48,7 +49,6 @@ def update_set(item_name, expiry_time, base_query=None, item_ids=None, get_item_
 		raise Exception("Unknown API")
 
 	# Save new data and remove data that is no longer present
-	# TODO:WV:20170629:Re-test this, especially for gravityforms
 	ids_before = get_item_ids(item_name)
 	if api_to_use == "woocommerce":
 		download_paginated_set(item_name, base_query, expiry_time)
@@ -85,9 +85,9 @@ def get_slugs_storage_key(item_name):
 
 def download_items_from_ids(item_name, expiry_time, item_ids, get_item_method):
 	for item_id in item_ids:
-
-		# TODO:WV:20170630:Handle failure here
 		item = get_item_method(item_id)
+		if not item:
+			raise Exception("Item not found")
 		storage.set(
 			get_single_item_storage_key(item_name, item_id),
 			item,
