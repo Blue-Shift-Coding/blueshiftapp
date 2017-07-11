e1b3d985a6b161c503897b1c763a86512c5b88e6
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-07-11 12:56:28 +0100

    Working on WP hook

--- wordpress-plugin/blueshift/blueshift.php
+++ wordpress-plugin/blueshift/blueshift.php
@@ -25,6 +25,9 @@ class BlueshiftPlugin {
 
 	public function preventAnyoneEditingTheFiltersCategory() {
 		add_action('edit_terms', function($termId, $termTaxonomyId, $taxonomySlug) {
+			if ($taxonomySlug == "product_cat") {
+
+			}
 			// TODO:WV:20170711:Check for taxonomy=product_cat and name is FILTERS and no parent product_cats.  If so, set a flash message and redirect to index.
 		});
 	}
